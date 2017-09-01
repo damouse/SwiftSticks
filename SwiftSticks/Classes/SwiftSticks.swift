fba87c69235ef2963b0c638c96badf3543264321
Mickey Barboi <mbarboi@lefthandrobotics.com>
2017-09-01 17:10:19 -0600

    top level API, top level uiview wrapper

--- SwiftSticks/Classes/SwiftSticks.swift
+++ SwiftSticks/Classes/SwiftSticks.swift
@@ -10,113 +10,49 @@ import Foundation
 import UIKit
 import SpriteKit
 
-
 public class StickView: SKView {
-//    private var skView: SKView!
+    var _scene: SKScene!
+    
+    // Instead of exposing accessors, just interact with the stick directly
+    public var stick: AnalogJoystick!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-//        skView = SKView(frame: frame)
-//        self.addSubview(skView)
-//         skView.bounds = self.bounds
-        
-        let scene = GameScene(size: self.bounds.size)
-        scene.backgroundColor = .white
-
-        showsFPS = true
-        showsNodeCount = true
-        ignoresSiblingOrder = true
-        presentScene(scene)
+        setupView()
     }
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder:aDecoder)
+        setupView()
+    }
+    
+    func setupView() {
+        _scene = SKScene(size: self.bounds.size)
+        _scene.backgroundColor = .white
+        _scene.view?.isMultipleTouchEnabled = true
+        _scene.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        stick = AnalogJoystick(diameter: 110)
+        stick.position = CGPoint(x: stick.radius + 15, y: stick.radius + 15)
+        stick.substrate.color = UIColor.random()
+        
+        _scene.addChild(stick)
+        
+        ignoresSiblingOrder = true
+        presentScene(_scene)
+    }
+    
+    // MARK: Analog Customization
+    open func setBaseColor(color: UIColor) {
+        stick.substrate.color = color
+    }
+    
+    open func setStickColor(color: UIColor) {
+        stick.stick.color = color
     }
 }
 
-
-class GameScene: SKScene {
-    var appleNode: SKSpriteNode?
-    
-    let moveAnalogStick = AnalogJoystick(diameter: 110)
-    let rotateAnalogStick = AnalogJoystick(diameter: 100)
-    
-    
-    var joystickStickImageEnabled = true {
-        didSet {
-            let image = joystickStickImageEnabled ? UIImage(named: "jStick") : nil
-            moveAnalogStick.stick.image = image
-            rotateAnalogStick.stick.image = image
-        }
-    }
-    
-    var joystickSubstrateImageEnabled = true {
-        didSet {
-            let image = joystickSubstrateImageEnabled ? UIImage(named: "jSubstrate") : nil
-            moveAnalogStick.substrate.image = image
-            rotateAnalogStick.substrate.image = image
-        }
-    }
-    
-    override func didMove(to view: SKView) {
-        backgroundColor = UIColor.white
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        
-        moveAnalogStick.position = CGPoint(x: moveAnalogStick.radius + 15, y: moveAnalogStick.radius + 15)
-        addChild(moveAnalogStick)
-        
-        rotateAnalogStick.position = CGPoint(x: self.frame.maxX - rotateAnalogStick.radius - 15, y:rotateAnalogStick.radius + 15)
-        addChild(rotateAnalogStick)
-        
-        
-        // These can move to the view controller as demos
-        //MARK: Handlers begin
-        moveAnalogStick.beginHandler = { [unowned self] in
-            guard let aN = self.appleNode else { return }
-            aN.run(SKAction.sequence([SKAction.scale(to: 0.5, duration: 0.5), SKAction.scale(to: 1, duration: 0.5)]))
-        }
-        
-        moveAnalogStick.trackingHandler = { [unowned self] data in
-            guard let aN = self.appleNode else { return }
-            aN.position = CGPoint(x: aN.position.x + (data.velocity.x * 0.12), y: aN.position.y + (data.velocity.y * 0.12))
-        }
-        
-        moveAnalogStick.stopHandler = { [unowned self] in
-            guard let aN = self.appleNode else { return }
-            aN.run(SKAction.sequence([SKAction.scale(to: 1.5, duration: 0.5), SKAction.scale(to: 1, duration: 0.5)]))
-        }
-        
-        rotateAnalogStick.trackingHandler = { [unowned self] jData in
-            self.appleNode?.zRotation = jData.angular
-        }
-        
-        rotateAnalogStick.stopHandler =  { [unowned self] in
-            guard let aN = self.appleNode else { return }
-            aN.run(SKAction.rotate(byAngle: 3.6, duration: 0.5))
-        }
-        // MARK: Handlers end
-        
-        joystickStickImageEnabled = true
-        joystickSubstrateImageEnabled = true
-        
-        addApple(CGPoint(x: frame.midX, y: frame.midY))
-        view.isMultipleTouchEnabled = true
-    }
-    
-    func addApple(_ position: CGPoint) {
-        guard let appleImage = UIImage(named: "apple") else { return }
-        
-        let texture = SKTexture(image: appleImage)
-        let apple = SKSpriteNode(texture: texture)
-        apple.physicsBody = SKPhysicsBody(texture: texture, size: apple.size)
-        apple.physicsBody!.affectedByGravity = false
-        apple.position = position
-        
-        addChild(apple)
-        appleNode = apple
-    }
-}
 
 extension UIColor {
     static func random() -> UIColor {
